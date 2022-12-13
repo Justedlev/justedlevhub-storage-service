71b6d8865e0215aced510a242d3a758b1c2b3e9b
Edward L <59051661+Justedlev@users.noreply.github.com>
2022-12-14 00:37:18 +0200

    -

--- src/main/java/com/justedlev/storage/boot/RootPathBoot.java
+++ src/main/java/com/justedlev/storage/boot/RootPathBoot.java
@@ -1,6 +1,8 @@
 package com.justedlev.storage.boot;
 
 import com.justedlev.storage.properties.JStorageProperties;
+import com.justedlev.storage.repository.FileRepository;
+
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.ApplicationArguments;
@@ -14,12 +16,14 @@ import java.nio.file.Files;
 @RequiredArgsConstructor
 public class RootPathBoot implements ApplicationRunner {
     private final JStorageProperties properties;
+    private final FileRepository fileRepository;
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
         if (Files.notExists(properties.getRootPath())) {
             Files.createDirectory(properties.getRootPath());
-            log.info("'{}' folder was created", properties.getRootPath());
+            fileRepository.deleteAll();
+            log.info("Created root directory: {}", properties.getRootPath().toAbsolutePath());
         }
     }
 }
