0773abf6db6c429032e5df503cee110e4761607f
Edward L <59051661+Justedlev@users.noreply.github.com>
2022-12-17 10:45:28 +0200

    -

--- storage-client/src/main/java/com/justedlev/storage/client/configuration/StorageFeignClientConfiguration.java
+++ storage-client/src/main/java/com/justedlev/storage/client/configuration/StorageFeignClientConfiguration.java
@@ -5,8 +5,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 
-import java.time.Duration;
-import java.time.temporal.ChronoUnit;
 import java.util.concurrent.TimeUnit;
 
 @RequiredArgsConstructor
